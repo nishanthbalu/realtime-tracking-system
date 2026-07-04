e319a561a693aa0d567ea484d319e655c167ed5f
nishanthbalu <nishanthbs49@gmail.com>
2026-07-04 16:05:00 +0530

    added java service and version update for kafka

---
+++ driver-service-java/src/test/java/com/tracking/driver_service_java/DriverServiceJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracking.driver_service_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverServiceJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
